e71ddc60fbfdb8c25503ecc0c687197eb6764c8f
FerPeke <ferna9752@gmail.com>
2021-07-13 21:46:14 -0500

    Conexion
    Creación de la conexión a la base de datos.

--- view/pagina-principal.php
+++ view/pagina-principal.php
@@ -131,48 +131,6 @@
           </div>
         </div>
       </div>
-
-      <div class="col-lg-4 col-xs-12 text-center">
-        <div class="box">
-          <div class="box-title">
-            <h3>Empleo 10</h3>
-          </div>
-          <div class="box-text">
-            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-          </div>
-          <div class="box-btn">
-            <a href="descripcion-trabajo">Learn More</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-xs-12 text-center">
-        <div class="box">
-          <div class="box-title">
-            <h3>Empleo 11</h3>
-          </div>
-          <div class="box-text">
-            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-          </div>
-          <div class="box-btn">
-            <a href="descripcion-trabajo">Learn More</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-xs-12 text-center">
-        <div class="box">
-          <div class="box-title">
-            <h3>Empleo 12</h3>
-          </div>
-          <div class="box-text">
-            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-          </div>
-          <div class="box-btn">
-            <a href="descripcion-trabajo">Learn More</a>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </div>
